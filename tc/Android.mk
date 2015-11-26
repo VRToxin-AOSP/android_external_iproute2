5fdfbee20fccd539a5ce239379c0f135c513374b
despairfactor <matthewdalex92@gmail.com>
2015-11-26 16:59:04 -0500

    tc: add support for pie fq.c prio.c hhf.c multiq.c mqprio.c codel.c

--- tc/Android.mk
+++ tc/Android.mk
@@ -6,7 +6,7 @@ LOCAL_CLANG := false
 LOCAL_SRC_FILES :=  tc.c tc_qdisc.c q_cbq.c tc_util.c tc_class.c tc_core.c m_action.c \
                     m_estimator.c tc_filter.c tc_monitor.c tc_stab.c tc_cbq.c \
                     tc_estimator.c f_u32.c m_police.c q_ingress.c m_mirred.c q_htb.c q_cake.c q_cake0.c q_cake2.c \
-					q_fq_codel.c tc_exec.c
+					q_fq_codel.c tc_exec.c q_pie.c q_fq.c q_prio.c q_hhf.c q_multiq.c q_mqprio.c q_codel.c
 
 LOCAL_MODULE := tc
 
@@ -19,7 +19,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
 
 LOCAL_CFLAGS := -O2 -g -W -Wall -Wno-pointer-arith -Wno-sign-compare -Werror \
     -Wno-unused-parameter \
-    -Wno-missing-field-initializers
+    -Wno-missing-field-initializers -Wno-type-limits
 
 # This is a work around for b/18403920
 LOCAL_LDFLAGS := -Wl,--no-gc-sections
