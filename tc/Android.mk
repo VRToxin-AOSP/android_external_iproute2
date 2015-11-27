0e8a8b5b25366af0b221f837721748cb44bda9ca
despairfactor <matthewdalex92@gmail.com>
2015-11-26 23:53:54 -0500

    tc: compile tc_red and remove codel, cbq and atm tc: build in support for more qdiscs
    Added:
    q_choke.c q_drr.c q_dsmark.c q_gred.c q_hfsc.c q_netem.c q_qfq.c q_red.c q_sfb.c q_sfq.c q_tbf.c tc_red.c

--- tc/Android.mk
+++ tc/Android.mk
@@ -6,7 +6,9 @@ LOCAL_CLANG := false
 LOCAL_SRC_FILES :=  tc.c tc_qdisc.c q_cbq.c tc_util.c tc_class.c tc_core.c m_action.c \
                     m_estimator.c tc_filter.c tc_monitor.c tc_stab.c tc_cbq.c \
                     tc_estimator.c f_u32.c m_police.c q_ingress.c m_mirred.c q_htb.c q_cake.c q_cake0.c q_cake2.c \
-					q_fq_codel.c tc_exec.c q_pie.c q_fq.c q_prio.c q_hhf.c q_multiq.c q_mqprio.c q_codel.c q_fq_pie.c
+		    q_fq_codel.c tc_exec.c q_pie.c q_fq.c q_prio.c q_hhf.c q_multiq.c q_mqprio.c q_fq_pie.c \
+		    q_choke.c q_codel.c q_drr.c q_dsmark.c q_gred.c q_hfsc.c q_netem.c q_qfq.c q_red.c \
+		    q_sfb.c q_sfq.c q_tbf.c tc_red.c
 
 LOCAL_MODULE := tc
 
